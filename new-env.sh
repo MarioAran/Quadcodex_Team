2ac168181cfb0d9ee01a111555380145dbde884b
Mario German <arancibigm@gmail.com>
2025-12-01 18:53:32 +0100

    new try dataset

--- new-env.sh
+++ new-env.sh
@@ -10,10 +10,14 @@ source .env/bin/activate
 #dowload gym recomendatio dataset
 #!/bin/bash
 
+
+#!/bin/bash
 URL="https://www.kaggle.com/api/v1/datasets/download/niharika41298/gym-exercise-data"
 # Archivo de salida (puedes cambiarle el nombre si quieres)
-OUTPUT="data/gym_recomendation.xlsx"
+OUTPUT="data/gym-exercise-data.zip"
 # Descargar archivo
 curl -L "$URL" -o "$OUTPUT"
+unzip "$OUTPUT" -d data/
+rm -rf $OUTPUT
 echo "Descarga completa. Archivo guardado en $OUTPUT"
 
